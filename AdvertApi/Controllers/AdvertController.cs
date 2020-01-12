84437e00d34975a0e0b6dbf06b2ab08ea545a799
Rails Udemy Course <enginbulut@live.com>
2020-01-12 16:26:20 +0300

    add create and comfirm end points

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService advertStorageService;
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            this.advertStorageService = advertStorageService;
+        }
+        [HttpPost]
+        [Route("create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await advertStorageService.Add(model);
+            }
+            catch(KeyNotFoundException ex)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            return new OkResult();
+        }
+    }
+}
